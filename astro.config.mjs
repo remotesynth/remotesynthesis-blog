6fb3e1be4d9315a3f07c38e223d93c23beb6f7df
Brian Rinaldi <brian.rinaldi@gmail.com>
2023-01-02 15:07:21 -0500

    favicons

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig(
     // outDir: './dist',       // When running `astro build`, path to final static output
     // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
 
-    site: "https://astro-ink.vercel.app",
+    site: "https://remotesynthesis.com",
     // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     server: {
       // port: 3000,         // The port to run the dev server on.
