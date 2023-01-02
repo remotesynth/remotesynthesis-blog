bb1051f958aab1c9969a6a7efa5d12a477af7574
Brian Rinaldi <brian.rinaldi@gmail.com>
2023-01-02 13:57:53 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 // Full Astro Configuration API Documentation:
@@ -16,47 +17,44 @@ const __dirname = dirname(__filename);
 // You can disable this by removing "@ts-check" and `@type` comments below.
 
 // @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
+
+// https://astro.build/config
+export default defineConfig( /** @type {import('astro').AstroUserConfig} */{
   // root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // outDir: './dist',       // When running `astro build`, path to final static output
   // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
 
-  site: "https://astro-ink.vercel.app", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: "https://astro-ink.vercel.app",
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   server: {
     // port: 3000,         // The port to run the dev server on.
   },
-  integrations: [
-    mdx(),
-    svelte(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    sitemap(),
-  ],
+  integrations: [mdx(), svelte(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap()],
   vite: {
     plugins: [],
     resolve: {
       alias: {
-        $: path.resolve(__dirname, "./src"),
-      },
+        $: path.resolve(__dirname, "./src")
+      }
     },
     optimizeDeps: {
-      allowNodeBuiltins: true,
-    },
+      allowNodeBuiltins: true
+    }
   },
   markdown: {
-    render: [
-      "@astrojs/markdown-remark",
-      {
-        syntaxHighlight: "shiki",
-        shikiConfig: {
-          theme: "nord",
-          langs: ["js", "html", "css", "liquid"],
-          wrap: false,
-        },
-      },
-    ],
+    render: ["@astrojs/markdown-remark", {
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "nord",
+        langs: ["js", "html", "css", "liquid"],
+        wrap: false
+      }
+    }]
   },
+  output: "server",
+  adapter: netlify()
 });
